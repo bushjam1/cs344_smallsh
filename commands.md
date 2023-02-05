0e09a2ecb64051a9876110a87474064c97a3776c
James Bush <bushjam@oregonstate.edu>
2023-02-05 15:10:38 -0800

    add commands.md

---
+++ commands.md
@@ -0,0 +1,2 @@
+## Full Compile 
+gcc -std=c99 -Wall -Wextra -Wpedantic -g -o smallsh smallsh.c
--- smallsh.c
+++ smallsh.c
@@ -4,30 +4,28 @@
 
 int main(){
   // Main loop
- 
   char *line = NULL;
   size_t n = 0;
-  ssize_t nread;
+  //ssize_t nread; ??
 
   for (;;) {
     /* Display prompt*/	
-    printf("%s",getenv("PS1")); 
+    printf("%s",getenv("PS1"));
     /* Get line of input from terminal */
-
     ssize_t line_length = getline(&line, &n, stdin); /* Reallocates line */
-    
-    /* Check for error */ 
+
+    /* Check for error */
     if (line_length == -1){
       free(line);
       perror("getline() failed");
       exit(EXIT_FAILURE);
-    };
+    }
 
     /* Process / write line */ 
     //fwrite(line, nread, 1, stdout);
     printf("Line: %s\n",line);
-    
   };
+
   /* Free buffer */
   free(line);
   exit(EXIT_SUCCESS);
